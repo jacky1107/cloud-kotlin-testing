6e2e336aa2f02a1e1059dcaf1a04373306194e72
Jacky Wang <https://github.com/jacky1107>
2022-07-13 00:59:08 +0800

    mission completed

--- src/main/kotlin/nextdrive/cloud/testing/MockFooFinder.kt
+++ src/main/kotlin/nextdrive/cloud/testing/MockFooFinder.kt
@@ -7,17 +7,21 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 class MockFooFinder : FooFinder {
 
+    private var target = ""
+    private fun findTarget(foo: List<Foo>, label: String) {
+        foo.forEach { item ->
+            if (item.label == label) target = item.value ?: target
+            item.children?.let { findTarget(it, label) }
+        }
+    }
+
     override fun find(label: String): String? {
-        var target = ""
         try {
             val jsonfile = File("src/main/resources/foo.json").readText()
             val mapper = jacksonObjectMapper()
             val foo: List<Foo> = mapper.readValue(jsonfile)
+            findTarget(foo, label)
 
-            foo.forEach { item ->
-                val temp = item.children?.find { it.label == label }
-                target = temp?.value ?: target
-            }
         } catch (ioException:IOException) {
             ioException.printStackTrace()
             return null
